aaf82f586fece9270371a0666cf167fedc222f1a
RyogaGrey <115057956+RyogaGrey@users.noreply.github.com>
2023-04-07 14:30:45 +0500

    Create main.java

---
+++ JavaLabs/Lab1/main.java
@@ -0,0 +1,13 @@
+public class Main{
+
+    // Case study #1. Introduction to programming environments. Designing programs of linear structure.
+    // Task 1 - Find the arithmetic mean and geometric mean of two numbers
+    // Task 2 - Calculation of the hypotenuse length of a right triangle by the lengths of the two cathetuses
+    // Task 3 - Calculation of functions
+
+    public static void main (String args[]) {
+        Task1.task();
+        Task2.task();
+        Task3.task();
+    }
+}
